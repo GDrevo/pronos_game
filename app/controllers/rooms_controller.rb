b260d28a7ece7a2801cf02e99c5e205060c2dc5f
guillaume drevo <guillaume.drevo@gmail.com>
2022-12-21 23:44:25 +0100

    work in progress

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,19 +3,19 @@ class RoomsController < ApplicationController
     @current_user = current_user
     redirect_to "home" unless @current_user
     @rooms = Room.public_rooms
-    @users = User.all_except(@current_user)
+    @users = @current_user.friends.all_except(@current_user)
     @room = Room.new
   end
 
-  def create
-    @room = Room.create(name: params["room"]["name"])
-  end
+  # def create
+  #   @room = Room.create(name: params["room"]["name"])
+  # end
 
   def show
     @current_user = current_user
     @single_room = Room.find(params[:id])
     @rooms = Room.public_rooms
-    @users = User.all_except(@current_user)
+    @users = @current_user.friends.all_except(@current_user)
     @room = Room.new
     @messages = @single_room.messages
     @message = Message.new
