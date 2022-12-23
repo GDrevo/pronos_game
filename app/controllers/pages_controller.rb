e4f578c54cc3175f5eeac8a7877fc9c37a8ab1f1
guillaume drevo <guillaume.drevo@gmail.com>
2022-12-24 00:55:53 +0100

    homepage avec classement fait

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @users = User.all
+    @users = User.all.sort_by { |user| -user.total_score }
+    @users = @users.first(100)
+    @new_users = User.last(20).reverse
   end
 end
